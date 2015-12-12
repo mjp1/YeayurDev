b02719ad065ee6029364a6393c72d57c5855ab6b
Matt Pierce <mjp1@yeayur.com>
2015-12-12 14:11:51 -0700

    DB Update

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST', '104.236.104.156'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', 'ocBb64zlfKki9Nq8he8W'),
