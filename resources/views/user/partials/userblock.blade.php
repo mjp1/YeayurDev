f3d76417cbbc175e20c577d779ce43bf989b1545
Matt Pierce <mjp1@yeayur.com>
2016-01-02 18:55:02 -0700

    Update Search Results Template

--- resources/views/user/partials/userblock.blade.php
+++ resources/views/user/partials/userblock.blade.php
@@ -1,6 +1,6 @@
 <div class="media">
 	<a class="pull-left" href="{{route('profile', ['username' => $user->username]) }}">
-		<img class="media-object" alt="{{ $user->getUsername() }}" src="">
+		<img class="media-object" alt="{{ $user->getUsername() }}" src="{{ asset('images/profiles') }}/{{ $user->getImagePath() }}">
 	</a>
 	<div class="media-body">
 		<h4 class="media-heading"><a href="{{route('profile', ['username' => $user->username]) }}">{{ $user->getUsername() }}</a></h4>
