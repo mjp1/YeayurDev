7e54229c65aa85401fe6ee07011ff0f34f21f6fc
Matt Pierce <mjp1@yeayur.com>
2016-05-01 14:28:33 -0700

    Fix Main Controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,6 +10,14 @@ use Yeayurdev\Models\User;
 
 class MainController extends Controller
 {
+
+	public function getPublicIndex()
+	{
+		$posts = Post::orderBy('created_at', 'desc')->paginate(30);
+		return view('main.public')
+			->with('posts', $posts);
+	}
+	
 	public function getDiscoverConnections()
 	{
 		$connectionsPosts = Post::where(function($query) {
