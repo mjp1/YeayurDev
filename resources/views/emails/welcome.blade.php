c7dbf013d8300b5a8d532b29cc8ee41b6521f75b
Matt Pierce <mjp1@yeayur.com>
2016-07-27 21:38:51 -0700

    Fix email

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -12,7 +12,7 @@
   <h4 style="font-weight:initial;margin-top:10px;">The Yeayur Team</h4>
   <div style="border-top:1px solid #e3e3e3;text-align:center;padding-top:15px;font-size:14px;">
     <ul style="list-style-type:none;margin:0px;padding:0px;">
-      <li style="display:inline;padding-right:10px;border-right:1px solid #e3e3e3;"><a href="{{ route('index') }}" target="_blank" style="text-decoration:none;color:#000;"></a>Yeayur</li>
+      <li style="display:inline;padding-right:10px;border-right:1px solid #e3e3e3;"><a href="{{ route('index') }}" target="_blank" style="text-decoration:none;color:#000;">Yeayur</a></li>
       <li style="display:inline;padding-left:8px;padding-right:10px;border-right:1px solid #e3e3e3;"><a href="{{ route('terms') }}" target="_blank" style="text-decoration:none;color:#000;">Terms of Service</a></li>
       <li style="display:inline;padding-left:8px;"><a href="{{ route('privacy') }}" target="_blank" style="text-decoration:none;color:#000;">Privacy Policy</a></li>
     </ul>
