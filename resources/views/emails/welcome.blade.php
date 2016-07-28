f698ee904f9d24fc6994e08b7a248a26d03c277c
Matt Pierce <mjp1@yeayur.com>
2016-07-27 21:36:36 -0700

    Update email styling

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,6 +1,6 @@
 <nav style="height:85px;position:absolute;top:0px;left:0px;width:100%;background-color:#ff3300;color:#fff;">
   <div style="max-width:600px;margin:auto;padding-left:15px;">
-    <img src="https://yeayur.com/images/logo_856469_web.png" style="width:45px;padding-top:22px;">
+    <img src="{{ asset('images/logo_856469_web.png') }}" style="width:45px;padding-top:22px;">
   </div>
 </nav>
 <div style="font-family:arial;color:#000;position:relative;top:100px;max-width:600px;margin:auto;padding:15px;">
@@ -12,9 +12,9 @@
   <h4 style="font-weight:initial;margin-top:10px;">The Yeayur Team</h4>
   <div style="border-top:1px solid #e3e3e3;text-align:center;padding-top:15px;font-size:14px;">
     <ul style="list-style-type:none;margin:0px;padding:0px;">
-      <li style="display:inline;padding-right:10px;border-right:1px solid #e3e3e3;">Yeayur</li>
-      <li style="display:inline;padding-left:8px;padding-right:10px;border-right:1px solid #e3e3e3;">Terms of Service</li>
-      <li style="display:inline;padding-left:8px;">Privacy Policy</li>
+      <li style="display:inline;padding-right:10px;border-right:1px solid #e3e3e3;"><a href="{{ route('index') }}" target="_blank" style="text-decoration:none;color:#000;"></a>Yeayur</li>
+      <li style="display:inline;padding-left:8px;padding-right:10px;border-right:1px solid #e3e3e3;"><a href="{{ route('terms') }}" target="_blank" style="text-decoration:none;color:#000;">Terms of Service</a></li>
+      <li style="display:inline;padding-left:8px;"><a href="{{ route('privacy') }}" target="_blank" style="text-decoration:none;color:#000;">Privacy Policy</a></li>
     </ul>
     <h5>&copy; 2016 - Yeayur, Inc.</h5>
   </div>
