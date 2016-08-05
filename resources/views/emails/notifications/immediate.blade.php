d82f35d1da20784bb6d2c0075ec34fb748b819fa
Matt Pierce <mjp1@yeayur.com>
2016-08-04 22:20:34 -0700

    Email update

--- resources/views/emails/notifications/immediate.blade.php
+++ resources/views/emails/notifications/immediate.blade.php
@@ -6,15 +6,15 @@
 <div style="font-family:arial;color:#000;position:relative;top:100px;max-width:600px;margin:auto;padding:15px;">
   @if ($poster->getImagePath() === "")
   @else
-  <img src="{{ $poster->getImagePath() }}" style="height:100px !important;border-radius:50%;float:left;margin-right:10px;margin-bottom:25px;"/>
+  <img src="{{ $poster->getImagePath() }}" style="height:100;border-radius:50%;float:left;margin-right:10px;margin-bottom:25px;"/>
   @endif
   <h2>{{ $poster->username }} has just posted on your profile!</h2>
   <span style="display:block;border-top:1px solid #e3e3e3;padding-top:15px;padding-bottom:15px;clear:both;"><?php echo $post->body ?></span>
   <div style="border-top:1px solid #e3e3e3;text-align:center;padding-top:15px;font-size:14px;">
     <ul style="list-style-type:none;margin:0px;padding:0px;">
-      <li style="display:inline;padding-right:10px;border-right:1px solid #e3e3e3;">Yeayur</li>
-      <li style="display:inline;padding-left:8px;padding-right:10px;border-right:1px solid #e3e3e3;">Terms of Service</li>
-      <li style="display:inline;padding-left:8px;"><a href="#" style="text-decoration:none;color:#000;">Privacy Policy</a></li>
+      <li style="display:inline;padding-right:10px;border-right:1px solid #e3e3e3;"><a href="{{ route('index') }}" target="_blank" style="text-decoration:none;color:#000;">Yeayur</a></li>
+      <li style="display:inline;padding-left:8px;padding-right:10px;border-right:1px solid #e3e3e3;"><a href="{{ route('terms') }}" target="_blank" style="text-decoration:none;color:#000;">Terms of Service</a></li>
+      <li style="display:inline;padding-left:8px;"><a href="{{ route('privacy') }}" target="_blank" style="text-decoration:none;color:#000;">Privacy Policy</a></li>
     </ul>
     <h5>&copy; 2016 - Yeayur, Inc.</h5>
   </div>
