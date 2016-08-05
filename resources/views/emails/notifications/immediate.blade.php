1bd35b5729157a9c4af1555beb4d43b9a0356ee9
Matt Pierce <mjp1@yeayur.com>
2016-08-04 22:24:45 -0700

    Email update

--- resources/views/emails/notifications/immediate.blade.php
+++ resources/views/emails/notifications/immediate.blade.php
@@ -9,7 +9,7 @@
   <img src="{{ $poster->getImagePath() }}" style="max-height:100px;border-radius:50%;float:left;margin-right:10px;margin-bottom:25px;"/>
   @endif
   <h2>{{ $poster->username }} has just posted on your profile!</h2>
-  <span style="display:block;border-top:1px solid #e3e3e3;padding-top:15px;padding-bottom:15px;clear:both;"><?php echo $post->body ?></span>
+  <span style="display:block;color:#000000;border-top:1px solid #e3e3e3;padding-top:15px;padding-bottom:15px;clear:both;"><?php echo $post->body ?></span>
   <div style="border-top:1px solid #e3e3e3;text-align:center;padding-top:15px;font-size:14px;">
     <ul style="list-style-type:none;margin:0px;padding:0px;">
       <li style="display:inline;padding-right:10px;border-right:1px solid #e3e3e3;"><a href="{{ route('index') }}" target="_blank" style="text-decoration:none;color:#000;">Yeayur</a></li>
