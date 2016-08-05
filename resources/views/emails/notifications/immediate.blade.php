9ca174d74b9c3ac72301c15535ea613cd35fa1e0
Matt Pierce <mjp1@yeayur.com>
2016-08-04 22:16:39 -0700

    Email update

--- resources/views/emails/notifications/immediate.blade.php
+++ resources/views/emails/notifications/immediate.blade.php
@@ -1,12 +1,12 @@
-<nav style="height:85px;position:absolute;top:0px;left:0px;width:100%;background-color:#ff3300;color:#fff;">
+<div style="height:85px;position:absolute;top:0px;left:0px;width:100%;background-color:#ff3300;color:#fff;">
   <div style="max-width:600px;margin:auto;padding-left:15px;">
     <img src="https://yeayur.com/images/logo_856469_web.png" style="width:45px;padding-top:22px;">
   </div>
-</nav>
+</div>
 <div style="font-family:arial;color:#000;position:relative;top:100px;max-width:600px;margin:auto;padding:15px;">
   @if ($poster->getImagePath() === "")
   @else
-  <img src="{{ $poster->getImagePath() }}" style="height:100px;border-radius:50%;float:left;margin-right:10px;margin-bottom:25px;"/>
+  <img src="{{ $poster->getImagePath() }}" style="height:100px !important;border-radius:50%;float:left;margin-right:10px;margin-bottom:25px;"/>
   @endif
   <h2>{{ $poster->username }} has just posted on your profile!</h2>
   <span style="display:block;border-top:1px solid #e3e3e3;padding-top:15px;padding-bottom:15px;clear:both;"><?php echo $post->body ?></span>
