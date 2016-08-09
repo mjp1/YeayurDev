5b134443aeadc2a9df82940ef510d9c73f688d28
Matt Pierce <mjp1@yeayur.com>
2016-08-08 20:51:54 -0700

    Weekly Email Update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \Yeayurdev\Console\Commands\Inspire::class,
+        \Yeayurdev\Console\Commands\WeeklyEmail::class,
     ];
 
     /**
